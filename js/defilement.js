9e011a29d88b8b7dbc0a1d3ffc542e8aea3c3a80
timetodev <devunity62400@gmail.com>
2019-03-07 16:30:43 +0100

    Update all file

--- js/defilement.js
+++ js/defilement.js
@@ -1,10 +1,15 @@
-function marqueelike() {
-    $('.messagedefilant').each(function() {
-      var texte = $(this).html();
-      $(this).html('<div><span>' + texte + '</span><span>' + texte + '</span></div>');
-    });
-  }
-   
-  $(window).on('load', function() {
-    marqueelike();
-  });
+var defile;// l'element a deplacer 
+var psinit = 580; // position horizontale de depart 
+var pscrnt = psinit; 		
+function texteDefile() { 
+   if (!defile) defile = document.getElementById('defile'); 
+   if (defile) { 
+      if(pscrnt < ( - defile.offsetWidth) ){ 
+         pscrnt = psinit; 
+                } else { 
+         pscrnt+= -2; // pixel par deplacement 
+      } 
+      defile.style.left = pscrnt+"px"; 
+   } 
+} 
+setInterval("texteDefile()",30); // delai de deplacement 
